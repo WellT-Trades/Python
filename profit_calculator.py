b4eff467547576a6dbf569ab5320d69023721360
WellT-Trades <wellingtondede73@gmail.com>
2026-07-04 23:32:14 +0100

    Build first profit calculator

--- profit_calculator.py
+++ profit_calculator.py
@@ -4,12 +4,12 @@
 currency = "AUDUSD"
 bias = "short"
 entry_price = 0.69371
-TP_price = 0.69112 #(TP means Take Profit)
-SL_price = 0.69431 #(SL means stop loss)
+tp_price = 0.69112 #(TP means Take Profit)
+sl_price = 0.69431 #(SL means stop loss)
 lot_size = 0.83
 
 # Calculate Profit = (Exit Price − Entry Price) × Lot Size
-Profit = (TP_price - entry_price) * lot_size
+profit = (entry_price - TP_price) * lot_size
 
 # Output
 print(currency, bias)
