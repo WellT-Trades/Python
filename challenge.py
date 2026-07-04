16819e0a97e77b47b3fa34b94ff9522f092ae3f7
WellT-Trades <wellingtondede73@gmail.com>
2026-07-04 21:43:04 +0100

    Build first Python trading risk calculator

---
+++ challenge.py
@@ -0,0 +1,20 @@
+# # Without looking anything up, write a Python program that:
+# Creates a variable called account_size and stores 1000.
+# Creates a variable called risk_percent and stores 2.
+# Calculates the risk amount using:
+# Risk Amount = Account Size × Risk Percent ÷ 100
+# Stores the result in a variable called risk_amount.
+# Prints:
+# Account Size: 1000
+# Risk Percent: 2
+# Risk Amount: 20
+
+account_size = 1000
+risk_percent = 2
+
+# Calculates the risk amount
+risk_amount = account_size * risk_percent / 100
+
+print("Account Size:", account_size)
+print("Risk Percent:", risk_percent)
+print("Risk Amount:",  risk_amount)
