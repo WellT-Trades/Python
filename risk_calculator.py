b253c1c66495dd12449a8f0445c146d8b4a39260
WellT <Wellingtondede73@gmail.com>
2026-07-17 15:39:20 +0100

    Improving risk calculator by implying the use of functions

--- risk_calculator.py
+++ risk_calculator.py
@@ -5,22 +5,23 @@
 # Risk Amount = Account Size × Risk Percent ÷ 100
 # Stores the result in a variable called risk_amount.
 # Prints:
-# Account Size: 1000
-# Risk Percent: 2
-# Risk Amount: 20
 
 account_size = 1000
-risk_percent = 5
+risk_percent = 2
 
 # Calculates the risk amount
-risk_amount = account_size * risk_percent / 100
+def calculate_risk(account_size, risk_percent):
+    risk_amount = account_size * (risk_percent/100)
+    return risk_amount
 
-if risk_percent > 2:
-    print("\nWarning: Risk exceeds your trading plan.")
+risk_amount = calculate_risk(account_size, risk_percent)
 
-else:
-    print("\nRisk is within your trading plan.\n")
+# if risk_percent > 2:
+#     print("\nWarning: Risk exceeds your trading plan.")
 
-print("Account Size:", account_size,  "\n")
-print("Risk Percent:", risk_percent, "\n")
-print("Risk Amount:",  risk_amount, "\n")
+# else:
+#     print("\nRisk is within your trading plan.\n")
+
+# print("Account Size:", account_size,  "\n")
+# print("Risk Percent:", risk_percent, "\n")
+print(f"Risk amount:{risk_amount}")
