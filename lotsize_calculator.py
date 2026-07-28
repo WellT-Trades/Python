455165e03681cd74287e242bb8b98722528306a1
WellT <Wellingtondede73@gmail.com>
2026-07-28 14:22:36 +0100

    REFACTOR PROGRAM WITH THE USE OF LOOPS

--- lotsize_calculator.py
+++ lotsize_calculator.py
@@ -1,35 +1,65 @@
 # LOTSIZE CALCULATOR
-account_size = float(input("Account Size: "))
-currency = input("Currency: ").upper()
-risk_percentage = float(input("Risk(%): "))
-sl_pips = float(input("SL Pips: "))
-pair_value = float(input("Pair Value: "))
 
-
-from risk_calculator import calculate_risk
+# IMPORTS
+from risk_calculator import calculate_risk_percent
+# FUNCTIONS
+def calculate_lot_size(risk_amount, sl_pips, pair_value):
+    lot_size = (risk_amount / sl_pips) / pair_value
+    return lot_size
 
 def format_summary(currency, account_size, sl_pips, risk_percentage, risk_amount, lot_size):
-    summary = (f"\n=== TRADE  SUMMARY ===\n Account Size: {account_size}\n Currency: {currency}\n SL: {sl_pips}\n Risk (%): {risk_percentage}% of {account_size}\n Risk Amount: {risk_amount}\n Lot size: {lot_size:.2f}\n")
+    summary = (f"{'=' * 20}\nTRADE  SUMMARY\n{'=' * 20}\nAccount Size: {account_size}\nCurrency: {currency}\nSL: {sl_pips}\nRisk (%): {risk_percentage}% of {account_size}\nRisk Amount: {risk_amount}\nLot size: {lot_size:.2f}\n")
     return(summary)
 
-# CONDITIONS
-if account_size <= 0:
-    print("\nError!: Trader must have capital.")
 
-elif risk_percentage <= 0:
-    print("\nError!: Risk must be greater than zero.")
+if __name__ == "__main__":
+    # INPUTS
+    account_size = float(input("Account Size: "))
+    currency = input("Currency: ").upper()
+    risk_amount = float(input("Risk Amount: "))
+    sl_pips = float(input("SL Pips: "))
+    pair_value = float(input("Pair Value: "))
 
-elif sl_pips <= 1:
-    print("\nError!: SL must be greater than one.")
+    # CONDITIONS
+    while account_size <= 9:
+        print("\nError!: Trader must have capital.")
+        account_size = float(input("Account Size: "))
 
-elif pair_value <= 0:
-    print("\nError!: Pair value must be greater than zero")
+    while risk_amount <= 0:
+        print("\nError!: Risk must be greater than zero.")
+        risk_amount = float(input("Risk Amount: "))
 
-else:
-    # CONVERSIONS
-    risk_amount = calculate_risk(account_size, risk_percentage)
+    while sl_pips <= 1:
+        print("\nError!: SL must be greater than one.")
+        sl_pips = float(input("SL Pips: "))
 
-    # LOT SIZE FORMULA
-    lot_size = (risk_amount / sl_pips) / pair_value
-     
-    print(format_summary(currency, account_size, sl_pips, risk_percentage,risk_amount, lot_size))
+    while pair_value <= 0:
+        print("\nError!: Pair value must be greater than zero")
+        pair_value = float(input("Pair Value: "))
+
+    else:
+        # CONVERSIONS
+        # RISK AMOUNT FORMULA
+        risk_percent = int(calculate_risk_percent(
+            account_size,
+            risk_amount
+            ))
+        
+        # LOT SIZE FORMULA
+        lot_size = calculate_lot_size(
+            risk_amount,
+            sl_pips,
+            pair_value
+        )
+        # SUMMARY OUTPUT
+        summary = format_summary(
+            currency,
+            account_size,
+            sl_pips,
+            risk_percent,
+            risk_amount,
+            lot_size
+        )
+        
+print(summary)
+        
