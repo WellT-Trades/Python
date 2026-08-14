ef4c07843448f818f5947603f7dc4d4ba2a611f4
WellT <Wellingtondede73@gmail.com>
2026-08-14 12:44:02 +0100

    hello to the world

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-<<<<<<< HEAD
-print("Hello, World!")
-=======
-# first file
-print("Hello, WellT!")
-name = input()
+# <<<<<<< HEAD
+# print("Hello, World!")
+# =======
+# # first file
+# print("Hello, WellT!")
+# name = input()
 
-print("Its gon be a good day. \nLet's get it!")
->>>>>>> c106a47 (Building password security with python)
+# print("Its gon be a good day. \nLet's get it!")
+# >>>>>>> c106a47 (Building password security with python)
+name = "import weather_service"
+print(name)
