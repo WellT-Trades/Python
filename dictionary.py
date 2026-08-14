85a95f35fd3ca6a506e1c98603fa78ec4d9b106b
WellT <Wellingtondede73@gmail.com>
2026-08-14 12:01:32 +0100

    learning dictionary in Python

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+pair_values = {
+    
+        "EURUSD": 10,
+        "GBPUSD": 10,
+        "AUDUSD": 10,
+        "USDJPY": 7.12,
+        "USDCAD": 7.3,
+    
+        "XAUUSD":10,
+        "XAGUSD":10,
+    
+        "S&P 500": 4.9
+    }
