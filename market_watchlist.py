57acfd4301e7177bee972c78d40014b467ea8ee1
WellT <Wellingtondede73@gmail.com>
2026-08-14 12:17:04 +0100

    Learning List in Python

---
+++ market_watchlist.py
@@ -0,0 +1,12 @@
+# MARKET WATCHLIST
+watchlist = [
+    "EURUSD",
+
+    "GBPUSD",
+
+    "USDJPY",
+]
+for pair in range(watchlist):
+    print(
+        f"{'=' * 10}\nWATCHLIST\n{'=' * 10}\n\nlen(watchlist[])"
+    )
